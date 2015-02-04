59d396415cc70eda5c2e5a2a249bcb8a6b205320
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-04 16:52:48 -0500

    Add Default Page To Point To All Contacts In Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-  resources :contact
-  
+  root "contacts#index"
+  resources :contacts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
