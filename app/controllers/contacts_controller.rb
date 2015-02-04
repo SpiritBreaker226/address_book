00c642acffc6d8afb549a485d47f578eb7c909ff
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-04 17:24:19 -0500

    Add Create Contact Form & Post Function

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,20 @@ class ContactsController < ApplicationController
 	def index
 		@contacts = Contact.all
 	end
+
+	def new
+		@contact = Contact.new	
+	end
+
+	def create
+		@contact = Contact.new(contact_pararm)
+
+		@contact.save ? redirect_to(contacts_url) : :new
+	end
+
+	private
+
+	def contact_pararm
+		params.require(:contact).permit(:first_name, :last_name, :address, :picture_url, :phone, :email, :age, :birthday)
+	end
 end
